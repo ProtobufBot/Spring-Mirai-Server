adc866f59105ba99c9af65dc094413e6c4d84f0d
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-10-08 20:19:26 +0800

    update starter

--- src/main/java/net/lz1998/pbbot/plugin/DemoPlugin.java
+++ src/main/java/net/lz1998/pbbot/plugin/DemoPlugin.java
@@ -9,14 +9,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DemoPlugin extends BotPlugin {
+
     @Override
     public int onPrivateMessage(@NotNull Bot bot, @NotNull OnebotEvent.PrivateMessageEvent event) {
         long userId = event.getUserId();
         Msg msg = Msg.builder()
                 .face(1)
-                .text("hello")
+                .text("hello1")
+                .text("hello2")
                 .image("https://www.baidu.com/img/flexible/logo/pc/result@2.png");
-        bot.sendPrivateMsg(userId, msg.build(), false);
+        bot.sendPrivateMsg(userId, msg, false);
         return MESSAGE_BLOCK;
     }
 }
